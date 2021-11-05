ff5f0ef19635c36ced9f248b4caf208610e51bf1
abu-rifat <rifat.cse4.bu@gmail.com>
2021-11-05 18:57:48 +0600

    edit gitignore

---
+++ stuGetSub.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	freopen("dataFile.io","r",stdin);
+	freopen("stuGetSub.out","w",stdout);
+	int stuCt, subCt;
+	cin>>stuCt>>subCt;
+	
+	string subject_list[subCt+5];
+	map<string,int>sub_map;
+	for(int i=0;i<subCt;i++){
+		cin>>subject_list[i];
+		sub_map[]
+	}
+	
+	int 
+	return 0;
+}
+
+
+
+/*
+10
+1 D 0
+2 C 0
+3 A 0
+4 G 0
+5 B 2 G C
+6 A 0
+7 F 1 G
+8 B 1 A
+9 D 4 A G B C
+10 E 2 D G
+*/
